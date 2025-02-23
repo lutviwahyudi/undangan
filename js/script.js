007412aa515aff20c1484fbc4eb5a3d646026210
Donlut <87699531+lutviwahyudi@users.noreply.github.com>
2025-02-23 09:57:26 +0700

    frist commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//===================== fungsi untuk memutar musik audio ==================
+    document.addEventListener("DOMContentLoaded", function () {
+            var audio = document.getElementById("myAudio");
+    var button = document.getElementById("bukaUndangan");
+
+            audio.play().then(() => {
+        button.textContent = "Pause Musik";
+            }).catch(error => {
+        console.log("Autoplay diblokir oleh browser:", error);
+            });
+
+    button.addEventListener("click", function () {
+
+        let home = document.getElementById("home");
+        document.body.style.overflow = "auto";
+        home.classList.add("show-home");
+        home.scrollIntoView({ behavior: "smooth" });
+
+                if (audio.paused) {
+        audio.play();
+    button.textContent = "Undangan terbuka";
+                } else {
+        audio.pause();
+    button.textContent = "Undangan";
+                }
+            });
+        });
+// ===========================================================================
+document.addEventListener("DOMContentLoaded", function () {
+    let homeSection = document.querySelector(".home");
+
+    let observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                homeSection.classList.add("show");
+            }
+        });
+    }, { threshold: 0.3 });
+
+    observer.observe(homeSection);
+});
+// ============================================================================
